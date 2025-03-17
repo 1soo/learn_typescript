d26b5543a6d3fd6eb6ce3a37ee03c102bde6f921
unknown <aaa4799@naver.com>
2025-03-17 13:03:26 +0900

    react 프로젝트에 redux 설치

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { JSX, useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, 증가, 감소, 맘대로증가 } from "./index";
+import { Dispatch } from "redux";
+
+let 박스: JSX.Element = <div>박스</div>;
 
 function App() {
+  const [user, setUser] = useState<string | number>("kim");
+  const 꺼내온거 = useSelector((state: RootState) => state);
+  const dispatch: Dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>안녕하십니까</h4>
+      <Profile name="철수" age="20"></Profile>
+      <div>{박스}</div>
+      <button
+        onClick={() => {
+          dispatch(증가());
+        }}
+      >
+        증가
+      </button>
+      {꺼내온거.counter.count}
+      <button
+        onClick={() => {
+          dispatch(감소());
+        }}
+      >
+        감소
+      </button>
+      <br />
     </div>
   );
+}
+
+function Profile(props: { name: string; age: string }): JSX.Element {
+  return <div>{props.name}입니다.</div>;
 }
 
 export default App;
--- my-project/src/index.tsx
+++ my-project/src/index.tsx
@@ -1,19 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { Provider } from "react-redux";
+import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
+
+const 초기값 = { count: 0, user: "kim" };
+
+/*function reducer(state = 초기값, action: any) {
+  if (action.type === "증가") {
+    return { ...state, count: state.count + 1 };
+  } else if (action.type === "감소") {
+    return { ...state, count: state.count - 1 };
+  } else {
+    return 초기값;
+  }
+}
+
+const store = createStore(reducer);*/
+
+// slice 문법의 좋은 점 (state + reducer)
+/*
+  1. 함수라 보기 좋음
+  2. reducer에서 state의 복사본을 만들 필요가 없다.
+*/
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: 초기값,
+  reducers: {
+    증가(state) {
+      state.count++;
+    },
+    감소(state) {
+      state.count--;
+    },
+    맘대로증가(state, action: PayloadAction<number>) {
+      state.count += action.payload;
+    },
+  },
+});
+
+export const { 증가, 감소, 맘대로증가 } = counterSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
